f3345597f28b8d5d4bed8a565a777f1d60c803b4
ziyangw <ziyang.wang123@gmail.com>
2017-06-29 16:03:52 -0400

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='python_agent',
+      version='0.1',
+      description='Python Agent created by Ziyang and Vinai',
+      url='https://github.com/ziyangw/python_agent',
+      author=['Vinai Rachakonda', 'Ziyang Wang'],
+      author_email=['ziyang.wang123@gmail.com', 'rachakondavinai@gmail.com'],
+      license='MIT',
+      packages=['python_agent'],
+      zip_safe=False)
