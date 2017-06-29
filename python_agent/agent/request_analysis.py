11184b5377970bca30cd782e31dcaa4c8996c71f
ziyangw <ziyang.wang123@gmail.com>
2017-06-29 17:00:59 -0400

    new pip version

--- python_agent/agent/request_analysis.py
+++ python_agent/agent/request_analysis.py
@@ -3,3 +3,7 @@ class RequestAnalysis(object):
 
     def __init__(self, app):
         self.app = app
+
+    def __call__(self, environ, start_response):
+        print("middleware install testing")
+        return self.app(environ, start_response)
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='python_agent',
-      version='0.1',
+      version='0.3',
       description='Python Agent created by Ziyang and Vinai',
       url='https://github.com/ziyangw/python_agent',
       author=['Vinai Rachakonda', 'Ziyang Wang'],
       author_email=['ziyang.wang123@gmail.com', 'rachakondavinai@gmail.com'],
       license='MIT',
-      packages=['python_agent'],
+      packages=['python_agent', 'python_agent/agent'],
       zip_safe=False)
