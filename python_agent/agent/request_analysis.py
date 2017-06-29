1c002928d26b4cb00323ee60b71cd4c50bdd006a
Vinai Rachakonda <rachakondavinai@gmail.com>
2017-06-29 17:08:26 -0400

    sample middleware

--- python_agent/agent/request_analysis.py
+++ python_agent/agent/request_analysis.py
@@ -5,5 +5,14 @@ class RequestAnalysis(object):
         self.app = app
 
     def __call__(self, environ, start_response):
-        print("middleware install testing")
-        return self.app(environ, start_response)
+        print(environ.get('REQUEST_METHOD'))
+        print(environ.get('PATH_INFO'))
+        print(environ.get('QUERY_STRING'))
+
+        def demo_start_response(status, headers, exc_info=None):
+            print(status)
+            headers.append(('DEMO', "DEMO"))
+            return start_response(status, headers, exc_info)
+
+        return self.app(environ, demo_start_response)
+
