0fe6aa085f3cfa3f4b3e5ebc27b66cd7eaeec6a3
ziyangw <ziyang.wang123@gmail.com>
2017-06-30 16:26:47 -0400

    iterate string info

--- python_agent/agent/request_analysis.py
+++ python_agent/agent/request_analysis.py
@@ -1,5 +1,6 @@
 from urlparse import parse_qs
 from webob import Request
+import os
 
 
 class RequestAnalysis(object):
@@ -7,24 +8,36 @@ class RequestAnalysis(object):
     def __init__(self, app):
         self.app = app
 
-    """
-    def __call__(self, environ, start_response):
-        try:
-            content = werkzeug.wsgi.get_input_stream(environ)
-            request_body = content.read()
-        except Exception, e:
-            print(str(e))
-            request_body = "0"
-        d = parse_qs(request_body)
-        print(environ.get('QUERY_STRING'))
-        print("this is parsed body %s" % d)
-
-        return self.app(environ, start_response)
-    """
-
     def __call__(self, environ, start_response):
         request = Request(environ)
         request.make_body_seekable()
         parsed_request = parse_qs(request.body)
-        print(parsed_request)
-        return self.app(environ, start_response)
+        request_environ = locals()
+
+        def response_analysis(status, headers, exc_info=None):
+            response_environ = locals()
+            print("---------------")
+            print("String objects from request that are garbage collected: ")
+            RequestAnalysis.compute_diff(request_environ, response_environ)
+            print("String objects created between request and response: ")
+            RequestAnalysis.compute_diff(response_environ, request_environ)
+            print("---------------")
+            return start_response(status, headers, exc_info)
+
+        return self.app(environ, response_analysis)
+
+    @staticmethod
+    def compute_diff(dict1, dict2):
+        key1 = set(dict1.keys())
+        key2 = set(dict2.keys())
+        diff = key1.difference(key2)
+        diff_dict = {key: dict1[key] for key in diff}
+        RequestAnalysis.iterate_print(diff, diff_dict)
+
+    @staticmethod
+    def iterate_print(key, diff):
+        if not hasattr(diff, '__iter__') or type(diff) in [list, tuple, str, file]:
+            print(key, diff)
+        else:
+            for key in diff:
+                RequestAnalysis.iterate_print(key, diff[key])
