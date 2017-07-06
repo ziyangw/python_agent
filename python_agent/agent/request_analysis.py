ad0fa3c8426b5e8fd25d9794877c638baece0c87
Vinai Rachakonda <rachakondavinai@gmail.com>
2017-07-06 15:49:53 -0400

    added basic structure for import hook. Currently optimized for render lol

--- python_agent/agent/request_analysis.py
+++ python_agent/agent/request_analysis.py
@@ -2,19 +2,34 @@ from urlparse import parse_qs
 from webob import Request
 from agent import *
 from memory_profiler import memory_usage
+from django.shortcuts import render
+# from patch import *
 import time
 import uuid
 import sys
 import inspect
-import dis
+from import_hooks import *
+
+
+def _render(original_execute, request, template_name, context=None, content_type=None,
+            status=None, using=None):
+    """
+    Returns a HttpResponse whose content is filled with the result of calling
+    django.template.loader.render_to_string() with the passed arguments.
+    """
+    print('hey!')
+    content = loader.render_to_string(template_name, context, request,
+                                      using=using)
+    return HttpResponse(content, content_type, status, context=context)
 
 
 class RequestAnalysis(object):
     def __init__(self, app):
         self.app = app
+        render = _render
 
     def __call__(self, environ, start_response):
-        print("------Request Start------")
+        # print("------Request Start------")
         start = time.time()
         request = Request(environ)
         request.make_body_seekable()
@@ -23,31 +38,44 @@ class RequestAnalysis(object):
         request_cls = inspect.getmembers(sys.modules[__name__], inspect.isclass)
         request_mem_usage = memory_usage()
 
+        # shortcut_modules = sys.modules['django.shortcuts']
+        # shortcut_modules.render = _render
+        # sys.modules['django.shortcuts'] = shortcut_modules
+
         def response_analysis(status, headers, exc_info=None):
+            # shortcut_modules = sys.modules['django.shortcuts']
+            # shortcut_modules.render = _render
+            # sys.modules['django.shortcuts'] = shortcut_modules
+
             response_environ = locals()
             # print("---------------")
+            request = Request(environ)
+            request.make_body_seekable()
+            parsed_request = parse_qs(request.body)
+            # print(request)
+            # print(parsed_request)
             # print("String objects from request that are garbage collected: ")
             # RequestAnalysis.compute_diff(request_environ, response_environ)
             # print("String objects created between request and response: ")
             # RequestAnalysis.compute_diff(response_environ, request_environ)
             # print("---------------")
-            print("String objects created: %d" % str.called)
+            # print("String objects created: %d" % str.called)
             str.called = 0
             response_id = str(uuid.uuid4())
             agent.response_holder.add(
                 response_id, {'request_environ': environ, 'status': status,
                               'headers': headers})
-            print("Response ID assigned: %s" % response_id)
+            # print("Response ID assigned: %s" % response_id)
             response_cls = inspect.getmembers(sys.modules[__name__], inspect.isclass)
             end = time.time()
             total_time = end - start
-            print("Total time taken: %s" % total_time)
+            # print("Total time taken: %s" % total_time)
             agent.file.write("Time: %s \n" % str(total_time))
             response_mem_usage = memory_usage()
             mem_used = response_mem_usage[0] - request_mem_usage[0]
-            print('Memory used: %s' % str(mem_used))
+            # print('Memory used: %s' % str(mem_used))
             agent.memory_logger.write("Mem Used: %s \n" % str(mem_used))
-            print("------Request End------")
+            # print("------Request End------")
             return start_response(status, headers, exc_info)
 
         return self.app(environ, response_analysis)
